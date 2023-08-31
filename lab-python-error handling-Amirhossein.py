d1a95fc77f41f507c57060a088d1c4bf911c84d5
Amir_h_Ahmadzade <a.h.a9372@gmail.com>
2023-08-31 22:02:08 +0200

    final version

--- lab-python-error handling-Amirhossein.py
+++ lab-python-error handling-Amirhossein.py
@@ -1,6 +1,6 @@
 products = ["t-shirt", "mug", "hat", "book", "keychain"]
 inventory = {}
-customer_orders = set()
+customer_orders = {}
 def input_handler_initialize_inventory(item):
     quantity = int()
     try:
@@ -21,35 +21,59 @@ def input_handler_calculate_total(item):
     except ValueError as error:
         print(f"*** error: {error} ***")
         input_price = input_handler_calculate_total(item)
-    #except:
-    #   print("invalid price.Please try again.")
-    #   input_handler_calculate_total(item)
     return input_price
+
+def input_handler_customer_order_name(handler_inventory):
+    
+    try:
+         input_order_name = input(f"What's your order? ")
+         if input_order_name not in handler_inventory.keys() :
+             raise ValueError("We don't have this product for sale.")
+    except ValueError as error:
+        print(f"*** error: {error} ***")
+        input_order_name = input_handler_customer_order_name(handler_inventory)
+    return input_order_name
+
+def input_handler_customer_order_num(handler_inventory,item):
+    
+    try:
+         input_order_num = int(input(f"How many of them? "))
+         if input_order_num < 0 :
+             raise ValueError("Invalid quantity.")
+         elif input_order_num > handler_inventory[item]:
+            raise ValueError(f"not enugh resources.{handler_inventory[item]} left")
+    except ValueError as error:
+        print(f"*** error: {error} ***")
+        input_order_num = input_handler_customer_order_num(handler_inventory, item)
+    return input_order_num   
    
 def initialize_inventory(products_list):
   
     inventory = {item:input_handler_initialize_inventory(item) for item in products_list}
     return inventory
 
+def get_customer_orders(inventory):
+    order_invetory = inventory.copy()
+    done_ordering = False
+    while not(done_ordering):
+        product = input_handler_customer_order_name(order_invetory)
+        num = input_handler_customer_order_num(order_invetory, product)
+        if product in customer_orders.keys():
+            customer_orders[product] += num
+        else:
+            customer_orders[product] = num
+        order_invetory[product] -= num 
+        if input("Was it all? (yes or no) ") == 'yes':
+              done_ordering = True
 
-def get_customer_orders():
-
-
-    number_of_orders = int(input("How many items would you like to order?"))
-    customer_orders = {input(f"what is the #{order_counter+1} order? ") for order_counter in range(number_of_orders)}
-    return customer_orders
+    return customer_orders, order_invetory
 
 def calculate_order_statistics(inventory, customers_orders):
-    total_products_ordered = len(customer_orders)
+    total_products_ordered = sum(customer_orders.values()) 
     total_products = sum(inventory.values())
     percentage_of_products_ordered = round(total_products_ordered / total_products , 2)
     order_stat = (total_products_ordered, percentage_of_products_ordered)
     return order_stat
-
-def update_inventory(inventory, customers_orders):
-   updated_inventory = {key:(value-1 if key in customers_orders else value) for (key,value) in inventory.items()}
-   updated_inventory = {key:value for (key,value) in updated_inventory.items() if updated_inventory[key] >0}
-   return updated_inventory
 
 def print_order_statistics(order_stat):
     print("Order Statistics:")
@@ -61,17 +85,17 @@ def print_updated_inventory(inventory):
     print("Updated inventory has:",'\n' ,output)
 
 def calculate_total_price(customer_orders):
-    prices = [input_handler_calculate_total(item) for item in customer_orders]
-    total_price = round(sum(prices), 2)
+    prices = [(item, input_handler_calculate_total(item)) for item in customer_orders.keys()]
+    total_price = sum( [product[1] * customer_orders[product[0]] for product in prices])
     print(f"Total price is: {total_price}")
 
 
 inventory = initialize_inventory(products)
-customer_orders = get_customer_orders()
+customer_orders, updated_inventory = get_customer_orders(inventory)
 order_statistic = calculate_order_statistics(inventory, customer_orders)
 print_order_statistics(order_statistic)
-inventory = update_inventory(inventory,customer_orders)
-print_updated_inventory(inventory)
+inventory = updated_inventory
+print_updated_inventory(updated_inventory)
 calculate_total_price(customer_orders)
 
 
